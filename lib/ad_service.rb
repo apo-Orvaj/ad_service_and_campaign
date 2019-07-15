3885415dacc64efa175328a32f39d74852d3b78c
Apoorva Jain <apoorva.jain3293@gmail.com>
2019-07-16 00:52:31 +0530

    Code styling changes

--- lib/ad_service.rb
+++ lib/ad_service.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'open-uri'
 require 'net/http'
 require 'json'
 
+#  Responsible for connecting with 3rd party Ad Service
 class AdService
-
-	def call
-		url = URI("https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df")
-		http = Net::HTTP.new(url.host, url.port)
-		http.use_ssl = true
-		request = Net::HTTP::Get.new(url.path)
-		response = http.request(request)
-		JSON.parse(response.body)
-	end
+  def call
+    url = URI('https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(url.path)
+    response = http.request(request)
+    JSON.parse(response.body)
+  end
 end
--- lib/ad_service_and_campaign.rb
+++ lib/ad_service_and_campaign.rb
@@ -1,4 +1,4 @@
-require "ad_service_and_campaign/version"
+require 'ad_service_and_campaign/version'
 require 'ad_service'
 require 'campaign'
 
@@ -6,43 +6,47 @@ module AdServiceAndCampaign
   @campaign_ids = []
 
   def self.call_ad_service
-  	ad_service = AdService.new
-  	ad_service.call
+    ad_service = AdService.new
+    ad_service.call
   end
 
   def self.initialize_campaigns(ads_count)
-  	ads_count.times do |index|
-  		job_id = rand(0..999999).to_s
-  		status = Campaign::STATUS_LIST[rand(0..2)]
-  		campaign = Campaign.new(job_id, status, (index+1).to_s , "test description #{job_id}")
-  		@campaign_ids << campaign.object_id
-  	end
+    ads_count.times do |index|
+      job_id = rand(0..999_999).to_s
+      status = Campaign::STATUS_LIST[rand(0..2)]
+      campaign = Campaign.new(job_id, status, (index+1).to_s, "test description #{job_id}")
+      @campaign_ids << campaign.object_id
+    end
   end
 
   def self.campaign_ids
-  	@campaign_ids
+    @campaign_ids
   end
 
   def self.compare
-	result = []
+    result = []
 
-	ads = AdServiceAndCampaign.call_ad_service["ads"]
-	AdServiceAndCampaign.initialize_campaigns(ads.count)
-	ads.each_with_index do |ad, index|
-		campaign_obj = ObjectSpace._id2ref(AdServiceAndCampaign.campaign_ids[index])
+    ads = AdServiceAndCampaign.call_ad_service['ads']
+    AdServiceAndCampaign.initialize_campaigns(ads.count)
+    ads.each_with_index do |ad, index|
+      campaign_obj = ObjectSpace._id2ref(AdServiceAndCampaign.campaign_ids[index])
 
-		discrepancies = []
-		unless ad["status"] == campaign_obj.status
-			discrepancies << {"status": {"remote": ad["status"], "local": campaign_obj.status}}
-		end
-		unless ad["description"] == campaign_obj.ad_description
-			discrepancies << {"description": {
-			"remote": ad["description"],
-			"local": campaign_obj.ad_description
-			}}
-		end
-		result << { "remote_reference": campaign_obj.external_reference, "discrepancies": discrepancies }
-	end
-	result
+      discrepancies = []
+      unless ad['status'] == campaign_obj.status
+        discrepancies << { 'status': {
+          'remote': ad['status'],
+          'local': campaign_obj.status
+        } }
+      end
+      unless ad['description'] == campaign_obj.ad_description
+        discrepancies << { 'description': {
+          'remote': ad['description'],
+          'local': campaign_obj.ad_description
+        } }
+      end
+      result << { 'remote_reference': campaign_obj.external_reference,
+                  'discrepancies': discrepancies }
+    end
+    result
   end
 end
